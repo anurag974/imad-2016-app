68a263f10832868f3ae407c9bc3d3483cc477b77
anurag974 <anuraggupta974@gmail.com>
2016-09-23 01:41:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 
 var element=document.getElementById('mainElement');
 element.innerHTML='new-value';
+
+//move the image
+
+var img=document.getElementById('img');
+img.onclick=function(){
+  img.style.marginLeft='100px';  
+};
