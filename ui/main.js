d62d8339bf2c1aa1613492945eca68e34bdc2bea
anurag974 <anuraggupta974@gmail.com>
2016-09-25 22:38:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,28 @@
-console.log('Loaded!');
+var button=document.getElementById('counter');
 
-//changing the text of main div
-
-var element=document.getElementById('mainElement');
-element.innerHTML='new-value';
-
-//move the image
-
-var img=document.getElementById('madi');
-
-var marginLeft=0;
-
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-
-img.onclick=function(){
-  var interval=setInterval(moveRight,50);  
+button.onclick=function()
+{
+  //create a request
+  var request=new XMLHttpRequest();
+  
+  //capture the response and store it in a vriable
+  request.onreadystatechange=function()
+  {
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+        //take some action
+        if(request.status===200)
+        {
+           var counter= request.responseText;
+           var span=document.getElementById('count');
+           span.innerHTML=counter.toString();
+        }
+    }
+  };
+  
+  //make a request
+  request.open('GET','http://anurag974.imad.hasura-app.io/counter',true);
+  request.send(null);
+  
+    
 };
