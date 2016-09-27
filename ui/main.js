d4725947bc2294e49e2d4cbecae4fd65d4355a06
anurag974 <anuraggupta974@gmail.com>
2016-09-27 20:06:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,3 +26,24 @@ button.onclick=function()
   
     
 };
+
+//submit name
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+  //make a request to the server and send the name
+  var names=['name1','name2','name3','name4'];
+  var list='';
+  for(var i=0;i<names.length;i++)
+  {
+      list+='<li>'+names[i]+'</li>';
+  }
+  
+  var ul=document.getElementById('namelist');
+  ul.innerHTML=list;
+  
+  //capture a list of names and render it as list
+  
+};
