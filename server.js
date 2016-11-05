d638d4bd7987598ef58b74d7c5553aa474448c90
anurag974 <anuraggupta974@gmail.com>
2016-11-06 00:50:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,124 +6,122 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles={
-    'article-one':{
-    title:'Article One | Narendra Modi',
-    heading:'Article One',
-    date:'Sept 5,2016',
-    content:`
-     <p>
-                this is the content for my first article. this is the content for my first article. this is the content for my first article.this is the content for my first article . this is the content for my first article
-            </p>
-            
-            <p>
-                this is the content for my first article. this is the content for my first article. this is the content for my first article.this is the content for my first article . this is the content for my first article
-            </p>
-            
-            <p>
-                this is the content for my first article. this is the content for my first article. this is the content for my first article.this is the content for my first article . this is the content for my first article
-            </p>
-  `
-    },
-    'article-two':{
-        title:'Article Two | Mukesh Ambani',
-    heading:'Article Two',
-    date:'Sept 10,2016',
-    content:`
-     <p>
-                this is the content for my second article. this is the content for my first article. this is the content for my first article.this is the content for my first article . this is the content for my first article
-            </p>
-            
-            <p>
-                this is the content for my second article. this is the content for my first article. this is the content for my first article.this is the content for my first article . this is the content for my first article
-            </p>
-            
-            <p>
-                this is the content for my second article. this is the content for my first article. this is the content for my first article.this is the content for my first article . this is the content for my first article
-            </p>
-            `
-    },
-    'article-three':{
-        
-        title:'Article Three | Ratan Tata',
-    heading:'Article Three',
-    date:'Sept 15,2016',
-    content:`
-     
-            <p>
-                this is the content for my second article. this is the content for my first article. this is the content for my first article.this is the content for my first article . this is the content for my first article
-            </p>
-            `
-    }
+  'article-one' :{
+  title:'this is anurag gupta',
+  heading:'aricle one',
+  date:'september 21',
+  content:`<p>
+          This is the content This is the content 
+         This is the content This is the content This is the content    
+         </p>
+         
+         <p>
+         This is the content This is the content 
+         This is the content This is the content This is the content 
+         </p>
+         
+         <p>
+         This is the content This is the content 
+         This is the content This is the content This is the content 
+         </p>`
+},
+  'article-two':{
+    title:'this is anurag gupta',
+  heading:'aricle two',
+  date:'september 21',
+  content:`<p>
+          we are in the second article   
+         </p>
+        `
+},
+   'article-three':{
+   title:'this is anurag gupta',
+  heading:'aricle Three',
+  date:'september 21',
+  content:`<p>
+          we are in the second article   
+         </p>
+        `
+}
 };
-
-function createTemplate(data)
-{
+function createTemp(data){
     var title=data.title;
     var date=data.date;
     var heading=data.heading;
     var content=data.content;
-
-var htmlTemplate=`
+var htmlTemp=`
 <html>
     <head>
         <title>
             ${title}
         </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
         <link href="/ui/style.css" rel="stylesheet" />
         
     </head>
-    
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        
-        <h3>
-            ${heading}
-        </h3>
-        
-        <div>
-            ${date}
-        </div>
-        
-        <div>
-            ${content}
-        </div>
-        </div>
-    </body>
+   
+ <body>
+     <div class="container">
+     <div>
+         <a href="/">Home</a>
+         
+     </div>
+     <hr/>
+     
+     <h3>
+         ${heading}
+     </h3>
+     <div>
+         ${date}
+     </div>
+     <div>
+         ${content}
+     </div>
+    </div>
 </html>
 
-
 `;
-return htmlTemplate;
+return htmlTemp;
 }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res)
+{
+    counter=counter+1;
+    res.send(counter.toString());
+}
+);
+
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+    res.send(createTemp(articles[articleName]));
 });
+
+ 
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names=[];
+app.get('submit-name/:name',function(req,res){
+    //get the name from the request
+    var name=req.params.name;
+    
+    names.push(name);
+    //JSON:javascript object notation
+    res.send(JSON.stringify(names));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
-});
+}
