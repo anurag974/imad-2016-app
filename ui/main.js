554d8c43e25bc6b7b6cd96171693dfa07be7c071
anurag974 <anuraggupta974@gmail.com>
2016-11-06 02:23:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,8 +30,7 @@ button.onclick=function()
 
 //submit name
 
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
   //create a request
@@ -58,6 +57,8 @@ submit.onclick=function(){
   };
   
   //make a request
+  var nameInput=document.getElementById('name');
+var name=nameInput.value;
   request.open('GET','http://anurag974.imad.hasura-app.io/submit-name?name='+name,true);
   request.send(null);
   
